c479b0ca59da8c3267571cd5e4da14b7eae85cda
Manel Rodriguez <mrodriguez@imim.es>
2009-01-20 10:36:45 +0100

    Cambiar lastname a librería

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
+/* main.c */
 #include <stdio.h>
-#include <string.h>
+
+#include "askname.h"
 
 int main(int argc, char **argv)
 {
 	char first[255], last[255];
 
-	printf("Enter your first name: ");
-	fgets(first, 255, stdin);
-	first[strlen(first)-1] = '\0'; /* remove the newline at the end */
+	askname(first, last);
 
-	/* esto es otro experimento */
-	printf("Hello %s!\n", name);
-	return 0;
+	printf("Hello, %s %s!\n", first, last);
+ 	return 0;
 }
