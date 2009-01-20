bc0de2bccad09f3bf5a58d636bcd91655c2842b2
Manel Rodriguez <mrodriguez@imim.es>
2009-01-20 10:19:52 +0100

    comentario experimento

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ int main(int argc, char **argv)
 	printf("Now enter your last name: ");
 	gets(last); /* buffer overflow? what's that? */
 
+	/* esto es un experimento */
 	printf("Hello, %s %s!\n", first, last);
  	return 0;
 }
